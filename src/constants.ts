168c744e3fcfc437d461b56a57e64b9e50817e74
Fabio Spampinato <spampinabio@gmail.com>
2024-02-27 22:18:23 +0000

    Using the new ellipsisWidth option, for slightly better performance

--- src/constants.ts
+++ src/constants.ts
@@ -2,9 +2,12 @@
 /* MAIN */
 
 const ANSI_RE = /[\x1B\x9B]/;
+
 const ELLIPSIS = '…';
+const ELLIPSIS_WIDTH = 1;
+
 const RESET = '\x1B[0m';
 
 /* EXPORT */
 
-export {ANSI_RE, ELLIPSIS, RESET};
+export {ANSI_RE, ELLIPSIS, ELLIPSIS_WIDTH, RESET};
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@
 /* IMPORT */
 
 import fastStringTruncatedWidth from 'fast-string-truncated-width';
-import {ANSI_RE, ELLIPSIS, RESET} from './constants';
+import {ANSI_RE, ELLIPSIS, ELLIPSIS_WIDTH, RESET} from './constants';
 import type {Options} from './types';
 
 /* MAIN */
 
 //TODO: Maybe detect where "RESET" is necessary more precisely
 
-const truncate = ( input: string, width: number, options?: { ellipsis?: string } ): string => {
+const truncate = ( input: string, width: number, options?: { ellipsis?: string, ellipsisWidth?: number } ): string => {
 
   const limit = width;
   const ellipsis = options?.ellipsis ?? ELLIPSIS;
-  const {index, ellipsed, truncated} = fastStringTruncatedWidth ( input, { limit, ellipsis } );
+  const ellipsisWidth = options?.ellipsisWidth ?? ( ellipsis === ELLIPSIS ? ELLIPSIS_WIDTH : undefined );
+  const {index, ellipsed, truncated} = fastStringTruncatedWidth ( input, { limit, ellipsis, ellipsisWidth } );
 
   if ( !truncated ) return input;
 
